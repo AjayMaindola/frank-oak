63018d5f4628c8f9736d953399db9f5373f512f1
AjayMaindola <ajay_maindola@yahoo.co.in>
2025-01-05 07:56:26 +0530

    ph menu work done

--- src/app/Componant/CommonComponent/Header.jsx
+++ src/app/Componant/CommonComponent/Header.jsx
@@ -5,46 +5,48 @@ import { HiMagnifyingGlass, HiOutlineUserCircle } from "react-icons/hi2";
 import { LiaShoppingBagSolid } from "react-icons/lia";
 import { LuMenu } from "react-icons/lu";
 import { CiSearch } from "react-icons/ci";
+import { IoChevronDownOutline } from "react-icons/io5";
 import { IoIosHeartEmpty } from "react-icons/io";
-import phmenueimg from"../../../../public/images/megamenueimg/PHmenue/Menu_tile.jpg"
+import phmenueimg from "../../../../public/images/megamenueimg/PHmenue/fleg.svg";
+import cad from "../../../../public/images/megamenueimg/PHmenue/cad.svg";
 import LoginModal from "@/app/Modal/LoginModal";
 import { IoIosClose } from "react-icons/io";
 import LogoImg from "../../../../public/images/logo.png";
 import FAQ, { FaqItem } from "../FAQ";
+// export start function header------------------>
 export default function Header() {
+  let [usdStatu, setUsdstatus] = useState(false);
   let [megaMenuShow, setMegaMenuShow] = useState(-1);
   let [faqShowHide, setFaqShowHide] = useState(false);
   const [openOffcanvas, setOpenOffcancas] = useState(false);
-
   let [loginModal, setLoginModal] = useState(false);
   return (
     <>
       {loginModal && (
         <LoginModal setLoginModal={setLoginModal} loginModal={loginModal} />
       )}
-
       <header className="w-[100%] bg-white items-center sticky top-0 md:shadow-xl gap-2 md:gap-0  grid grid-cols-[55%_auto] md:grid md:grid-cols-[70%_auto] px-[15px] lg:px-[30px] py-[15] md:py-0  ">
         <div className=" sm:grid sm:grid-cols-[50%_auto] md:grid md:grid-cols-[20%_auto] items-center justify-start   ">
           <div className=" flex items-center  ">
-            <div>
+            <div className="">
               <LuMenu
                 onClick={() => setOpenOffcancas(!openOffcanvas)}
                 className="text-[25px] md:hidden"
               />
               <div
                 onClick={() => setOpenOffcancas(false)}
-                className={`w-[100%] bg-black opacity-50    fixed top-0 left-0 h-screen ${
+                className={`w-[100%] bg-black opacity-50 h-[100%] overflow-y-auto    fixed top-0 left-0 ${
                   openOffcanvas ? "block" : "hidden"
                 } `}
               >
                 {" "}
               </div>
               <div
-                className={`w-[70%] h-screen transition-all duration-500 bg-white fixed top-0 ${
+                className={`w-[70%] h-[100%] overflow-y-auto transition-all duration-500 bg-white fixed top-0 ${
                   openOffcanvas ? "left-0" : "left-[-80%]"
                 }`}
               >
-                <div className="border border-red-600  bg-[#daff54] pb-[40px] ">
+                <div className="border   bg-[#daff54] pb-[40px] ">
                   <div className="px-[15px] py-[5px] flex items-center justify-between bg-white">
                     <CiSearch className=" text-[20px] " />
                     <IoIosClose
@@ -72,37 +74,112 @@ export default function Header() {
                     </div>
                   </div>
                   <div className="mt-3">
-                  <FAQ>
-                    <FaqItem  title="Womens Collection">
-                      <ul className="px-[8px]">
-                        <li>Shop All</li>
-                        <li>Jackets</li>
-                        <li>Sweters</li>
-                        <li>Tops</li>
-                        <li>Bottoms</li>
-                      </ul>
-                    </FaqItem>
-                    <FaqItem title="Womens Collection">
-                      <ul className="px-[8px]">
-                        <li>Shop All</li>
-                        <li>Jackets</li>
-                        <li>Sweters</li>
-                        <li>Tops</li>
-                        <li>Bottoms</li>
-                      </ul>
-                    </FaqItem>
-                  </FAQ>
+                    <FAQ>
+                      <FaqItem title="Womens Collection">
+                        <ul className="px-[8px]">
+                          <li>Shop All</li>
+                          <li>Jackets</li>
+                          <li>Sweters</li>
+                          <li>Tops</li>
+                          <li>Bottoms</li>
+                        </ul>
+                      </FaqItem>
+                      <FaqItem title="Womens Collection">
+                        <ul className="px-[8px]">
+                          <li>Shop All</li>
+                          <li>Jackets</li>
+                          <li>Sweters</li>
+                          <li>Tops</li>
+                          <li>Bottoms</li>
+                        </ul>
+                      </FaqItem>
+                    </FAQ>
                   </div>
                   <div className=" max-w-[95%] mx-auto  h-[120px] object-contain mt-4 bg-[url('/images/megamenueimg/PHmenue/Menu_tile.jpg')] bg-center bg-cover">
-                 <h1 className="text-white text-[17px] px-[30px] pt-[80px]">Women's Witer Sale</h1>
-                 </div>
-                 <div className=" max-w-[95%] mx-auto  h-[120px] object-contain mt-4 bg-[url('/images/megamenueimg/PHmenue/Menu-male.jpg')] bg-center bg-cover">
-                 <h1 className="text-white text-[17px] px-[30px] pt-[80px]">Men's Witer Sale</h1>
-                 </div>
+                    <h1 className="text-white text-[17px] px-[30px] pt-[80px]">
+                      Women's Witer Sale
+                    </h1>
+                  </div>
+                  <div className=" max-w-[95%] mx-auto  h-[120px] object-contain mt-4 bg-[url('/images/megamenueimg/PHmenue/Menu-male.jpg')] bg-center bg-cover">
+                    <h1 className="text-white text-[17px] px-[30px] pt-[80px]">
+                      Men's Witer Sale
+                    </h1>
+                  </div>
                 </div>
-               
+                <div
+                  onClick={() => setUsdstatus(!usdStatu)}
+                  className="w-[100%]  bg-slate-200 py-1 px-[15px]"
+                >
+                  <div className="flex items-center">
+                    <Image
+                      className="mr-[5px]"
+                      src={phmenueimg}
+                      width="60px"
+                      height="30px"
+                      alt="img1"
+                    />
+                    <h1 className="mr-[5px]">$USD</h1>
+
+                    <IoChevronDownOutline />
+                  </div>
+                </div>
+                <div
+                  className={` bg-slate-200 ${
+                    usdStatu == true ? "hidden" : "block"
+                  }`}
+                >
+                  <div className="px-[25px] pt-[15px] flex item ">
+                    <Image
+                      className="mr-[5px]"
+                      src={cad}
+                      width="60px"
+                      height="30px"
+                      alt="img1"
+                    />
+                    <h1 className="mr-[5px]">$CAD</h1>
+                  </div>
+                  <div className="px-[25px] mt-[18px] pb-[15px] flex item ">
+                    <Image
+                      className="mr-[5px]"
+                      src={phmenueimg}
+                      width="60px"
+                      height="30px"
+                      alt="img1"
+                    />
+                    <h1 className="mr-[5px]">$USD</h1>
+                  </div>
+                </div>
+                  <FAQ>
+                  <FaqItem title="About-Us">
+                        <ul className="px-[8px]">
+                          <li>Who We Are</li>
+                          <li>Jackets</li>
+                          <li>Sweters</li>
+                          <li>Tops</li>
+                          <li>Bottoms</li>
+                        </ul>
+                      </FaqItem>
+                      <FaqItem title="About-Us">
+                        <ul className="px-[8px]">
+                          <li>Who We Are</li>
+                          <li>Jackets</li>
+                          <li>Sweters</li>
+                          <li>Tops</li>
+                          <li>Bottoms</li>
+                        </ul>
+                      </FaqItem>
+                      <FaqItem title="About-Us">
+                        <ul className="px-[8px]">
+                          <li>Who We Are</li>
+                          <li>Jackets</li>
+                          <li>Sweters</li>
+                          <li>Tops</li>
+                          <li>Bottoms</li>
+                        </ul>
+                      </FaqItem>
+                  </FAQ>
+
               </div>
-             
             </div>
             <div>
               <Image src={LogoImg} width="60px" height="30px" alt="img1" />
--- src/app/Componant/FAQ.jsx
+++ src/app/Componant/FAQ.jsx
@@ -2,41 +2,48 @@
 import React, { Children, useState } from "react";
 import { FaMinus, FaPlus } from "react-icons/fa6";
 
-export default function FAQ({children}) {
-    const [faqShowIndex, setFaqShowIndex]=useState(null) // 0 pass to null value when open 1st value
+export default function FAQ({ children }) {
+  const [faqShowIndex, setFaqShowIndex] = useState(null); // 0 pass to null value when open 1st value
   return (
     <>
       {React.Children.map(children, (child, index) => {
-        return React.cloneElement (child, {faqShowIndex, setFaqShowIndex, index})
+        return React.cloneElement(child, {
+          faqShowIndex,
+          setFaqShowIndex,
+          index,
+        });
       })}
-    
     </>
   );
 }
+export const FaqItem = ({
+  faqShowIndex,
+  setFaqShowIndex,
+  index,
+  title,
+  children,
+}) => {
+  const faqClick = (index) => {
+    setFaqShowIndex(faqShowIndex === index ? null : index);
+  };
 
-
-
-export const FaqItem = ({faqShowIndex, setFaqShowIndex, index, title, children}) => {
-
-    const faqClick = (index) => {
-        setFaqShowIndex(faqShowIndex === index ? null : index)
-    }
-
-    const faqOpen = faqShowIndex === index
+  const faqOpen = faqShowIndex === index;
 
   return (
     <div className="mt-[10px] px-[10px]  ">
-      <div  onClick={() => faqClick(index)}
-      className="flex items-center justify-between px-[8px] relative">
+      <div
+        onClick={() => faqClick(index)}
+        className="flex items-center justify-between px-[8px] relative"
+      >
         <h1>{title}</h1>
         {faqOpen ? <FaMinus /> : <FaPlus />}
-        
       </div>
       <div
         className={`w-[100%] overflow-hidden transition-all duration-500  ${
-            faqOpen ? " h-[150px]" : "h-0"
+          faqOpen ? " h-[150px]" : "h-0"
         }`}
-      >{children}
+      >
+        {children}
       </div>
     </div>
   );
