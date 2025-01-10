c2207541cfcc3ecad613f4503ebfae2290c3d013
AjayMaindola <ajay_maindola@yahoo.co.in>
2025-01-10 19:09:13 +0530

    center catgorey complite with responsive

--- src/app/Componant/PageComponent/MadeforGoodLiving.jsx
+++ src/app/Componant/PageComponent/MadeforGoodLiving.jsx
@@ -3,52 +3,32 @@ import Image from "next/image";
 export default function MadeforGoodLiving() {
   return (
     <>
-      <div className="w-full md:px-[10px] md:grid md:grid-cols-[50%_50%] mt-0 md:mt-[40px]  ">
-        <div className="mt-[20px]  md:mt-[70px]">
-
-          <div className="mt-[30px] md:pt-[0px] lg:pt-[20px]">
-            <h1 className=" text-[30px] font-semibold md:mt-[80px] lg:mt-[50px] md:text-[30px] lg:text-[40px] font-Poppins text-center">Made for Good Living.</h1>
-            <p className=" mt-[30px] text-[18px] px-[30px] md:pt-[30px] md:text-[18px] md:text-justify md:px-[40px]  lg:pt-[50px]  tracking-wide text-justify lg:text-[22px]  lg:px-[100px] md:leading-[35px] lg:leading-[45px] font-medium">
-            Born and raised in Montreal, Frank And Oak is dedicated to creating conscious collections that seamlessly fit into your everyday life.
-            </p>
-            <h1 className="text-[20px] px-[30px] mt-[25px] mb-[30px] md:mb-0 lg:pt-[20px] md:pt-[35px]  lg:px-[100px] md:px-[35px] lg:text-[30px] md:text-[25px] font-bold font-Roboto underline underline-offset-4">Lern More</h1>
-          </div>
-
-
-
+      <div className="w-full px-[10px] md:px-[10px] md:grid md:grid-cols-[50%_50%] mt-8 ">
+        <div className=" max-w-[1320px] mx-auto   ">
+          <h1 className="font-bold text-[20px] text-center py-5  md:py-10 lg:py-14 mb-5  lg:text-start lg:px-6 lg:text-[25px] xl:px-14">
+            Made for Good Living.
+          </h1>
+          <p className=" md:py-5 lg:py-8 text-justify px-6 text-[16px] md:text-[18px] lg:text-[22px] xl:px-14 ">
+            Born and raised in Montreal, Frank And Oak is dedicated to creating
+            conscious collections that seamlessly fit into your everyday life.
+          </p>
+          <h1 className=" py-10 px-6 pb-5 text-[20px] font-bold lg:text-[22px] lg:mt-8 cursor-pointer underline xl:px-14">
+            Lern More
+          </h1>
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-        <div className="px-[15px] md:px-[10px] md:grid md:grid-cols-2  md:gap-4 ">
-          <div className=" mb-[20px] md:mb-0  h-[375px] bg-[url('/images/MadeforGoodLiving/MadeforGood1.jpg')] bg-no-repeat bg-bottom bg-cover  ">
-            <div className="pt-[300px] md:pt-0 mt-0 md:mt-[300px] text-center text-white font-Ysabeau text-[25px] font-bold">
-              <h1 className="">Sustainable Practices</h1>
-            </div>
+        {/* Image section */}
+        <div className="  grid grid-cols-2  gap-3 lg:gap-5  ">
+          <div className=" z-20 relative h-[269px] bg-[url('/images/MadeforGoodLiving/MadeforGood1.jpg')] bg-no-repeat bg-bottom bg-cover">
+          <h1 className="md:text-[16px] lg:text-[20px] text-white absolute  bottom-3 left-1 md:left-3 lg:left-3"> Sustainable Practices</h1>
           </div>
-          <div className="mb-[20px] md:mb-0 h-[375px] bg-[url('/images/MadeforGoodLiving/MadeforGood2.jpg')] bg-no-repeat bg-cover  ">
-            <div className="pt-[300px] md:pt-0 mt-0 md:mt-[300px] text-center text-white font-Ysabeau text-[25px] font-bold">
-              <h1>Design Philosophy</h1>
-            </div>
+          <div className= " relative h-[269px] bg-[url('/images/MadeforGoodLiving/MadeforGood2.jpg')] bg-no-repeat bg-cover ">
+          <h1 className="md:text-[16px] lg:text-[20px] text-white absolute  bottom-3 left-1 md:left-3 lg:left-3"> Design Philosophy</h1>
           </div>
-          <div className="mb-[20px] md:mb-0 h-[375px] bg-[url('/images/MadeforGoodLiving/MadeforGood3.jpg')] bg-no-repeat bg-cover  ">
-            <div className="pt-[300px] md:pt-0 mt-0 md:mt-[300px] text-center text-white font-Ysabeau text-[25px] font-bold">
-              <h1>Fabrics Innovation</h1>
-            </div>
+          <div className=" relative h-[269px] bg-[url('/images/MadeforGoodLiving/MadeforGood3.jpg')] bg-no-repeat bg-cover ">
+          <h1 className="md:text-[16px] lg:text-[20px] text-white absolute  bottom-3 left-1 md:left-3 lg:left-3"> Fabrics Innovation</h1>
           </div>
-          <div className="mb-[20px] md:mb-0 h-[375px] bg-[url('/images/MadeforGoodLiving/MadeforGood4.webp')] bg-no-repeat bg-cover  ">
-            <div className="pt-[300px] md:pt-0 mt-0 md:mt-[300px] text-center text-white font-Ysabeau text-[25px] font-bold">
-              <h1>Partners and Factories</h1>
-            </div>
+          <div className=" relative h-[269px] bg-[url('/images/MadeforGoodLiving/MadeforGood4.webp')] bg-no-repeat bg-cover ">
+          <h1 className="md:text-[16px] lg:text-[20px] text-white absolute  bottom-3 left-1 md:left-3 lg:left-3"> Partners and Factories </h1>
           </div>
         </div>
       </div>
